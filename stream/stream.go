9517561134c356e38b171be01ab3fdfd73d1fcaf
Alexander Klizhentas <klizhentas@gmail.com>
2014-12-11 15:34:59 -0800

    More docs for streaming middleware

--- stream/stream.go
+++ stream/stream.go
@@ -1,11 +1,32 @@
-// package stream provides a special http.Handler middleware that solves several problems when dealing with http requests:
-//
-// * Reads the entire request and response into buffer, optionally buffering it to disk for large requests
-//
-// * Checks the limits for the requests and responses, rejecting in case if the limit was exceeded
-//
-// * Changes request content-transfer-encoding from chunked and provides total size to the handlers
-//
+/* package stream provides http.Handler middleware that solves several problems when dealing with http requests:
+
+* Reads the entire request and response into buffer, optionally buffering it to disk for large requests
+
+* Checks the limits for the requests and responses, rejecting in case if the limit was exceeded
+
+* Changes request content-transfer-encoding from chunked and provides total size to the handlers
+
+Examples of a streaming middleware:
+
+    handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hello"))
+	})
+
+    // Stream will read the body in buffer before passing the request to the handler
+    // calculate total size of the request and transform it from chunked encoding before passing to the server
+    stream.New(handler)
+
+   // This version will buffer up to 2MB in memory and will serialize any extra to a temporary file
+   // if the request size exceeds 10MB it will reject the request
+   stream.New(handler, MemRequestBodyBytes(2 * 1024 * 1024), MaxRequestBodyBytes(10 * 1024 * 104))
+
+   // Will do the same as above, but with responses
+   stream.New(handler, MemResponseBodyBytes(2 * 1024 * 1024), MaxResponseBodyBytes(10 * 1024 * 104))
+
+  // Stream will replay the request if the handler returns error at least 3 times before returning the response
+  stream.New(handler, `IsNetworkError() && Attempts() <= 2`)
+
+*/
 package stream
 
 import (
