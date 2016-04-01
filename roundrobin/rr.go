01c711b5c6655522434b1164690e782020dc9d54
Owen Marshall <owen.marshall@ge.com>
2016-03-31 22:29:32 -0400

    qnd cookie hacking

--- roundrobin/rr.go
+++ roundrobin/rr.go
@@ -71,6 +71,32 @@ func (r *RoundRobin) Next() http.Handler {
 	return r.next
 }
 
+func (r *RoundRobin) getCookieVal(req *http.Request) (*url.URL, bool, error) {
+	// Before we move on to a new server, peek at our req cookie to see if we are bound.
+	// If so, serve to them.
+	cookie, err := req.Cookie("__STICKY_SVR")
+	switch err {
+	case nil:
+	case http.ErrNoCookie:
+		return nil, false, nil
+	default:
+		return nil, false, err
+
+	}
+
+	s_url, err := url.Parse(cookie.Value)
+	if err != nil {
+		return nil, false, err
+	}
+
+	s, i := r.findServerByURL(s_url)
+	if i != -1 {
+		return s.url, true, nil
+	} else {
+		return nil, false, nil
+	}
+
+}
 func (r *RoundRobin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if log.GetLevel() >= log.DebugLevel {
 		logEntry := log.WithField("Request", utils.DumpHttpRequest(req))
@@ -78,20 +104,30 @@ func (r *RoundRobin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		defer logEntry.Debug("vulcand/oxy/roundrobin/rr: competed ServeHttp on request")
 	}
 
-	url, err := r.NextServer()
+	// make shallow copy of request before chaning anything to avoid side effects
+	newReq := *req
+	cookie_url, present, err := r.getCookieVal(&newReq)
+
 	if err != nil {
 		r.errHandler.ServeHTTP(w, req, err)
 		return
+	}
+
+	if present {
+		newReq.URL = cookie_url
+	} else {
+		url, err := r.NextServer()
+		if err != nil {
+			r.errHandler.ServeHTTP(w, req, err)
+			return
+		}
+		newReq.URL = url
 	}
 
 	if log.GetLevel() >= log.DebugLevel {
 		//log which backend URL we're sending this request to
 		log.WithFields(log.Fields{"Request": utils.DumpHttpRequest(req), "ForwardURL": url}).Debugf("vulcand/oxy/roundrobin/rr: Forwarding this request to URL")
 	}
-
-	// make shallow copy of request before chaning anything to avoid side effects
-	newReq := *req
-	newReq.URL = url
 
 	//Emit event to a listener if one exists
 	if r.requestRewriteListener != nil {
