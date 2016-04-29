bd1185a2f66d66210343482e69bd555c20eee384
kevin <kevin@goguardian.com>
2016-04-29 15:54:22 -0700

    Fix deadlock with Append in roundtrip.go

--- memmetrics/histogram_test.go
+++ memmetrics/histogram_test.go
@@ -3,6 +3,7 @@ package memmetrics
 import (
 	"time"
 
+	"github.com/codahale/hdrhistogram"
 	"github.com/mailgun/timetools"
 	. "gopkg.in/check.v1"
 )
@@ -124,4 +125,60 @@ func (s *HistogramSuite) TestReset(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(m.ValueAtQuantile(100), Equals, int64(5))
 
+}
+
+func (s *HistogramSuite) TestHDRHistogramExportReturnsNewCopy(c *C) {
+	// Create HDRHistogram instance
+	a := HDRHistogram{}
+	a.low = 1
+	a.high = 2
+	a.sigfigs = 3
+	a.h = hdrhistogram.New(0, 1, 2)
+
+	// Get a copy and modify the original
+	b := a.Export()
+	a.low = 11
+	a.high = 12
+	a.sigfigs = 4
+	a.h = nil
+
+	// Assert the copy has not been modified
+	c.Assert(b.low, Equals, int64(1))
+	c.Assert(b.high, Equals, int64(2))
+	c.Assert(b.sigfigs, Equals, 3)
+	c.Assert(b.h, NotNil)
+}
+
+func (s *HistogramSuite) TestRollingHDRHistogramExportReturnsNewCopy(c *C) {
+	a := RollingHDRHistogram{}
+	a.idx = 1
+	origTime := time.Now()
+	a.lastRoll = origTime
+	a.period = 2 * time.Second
+	a.bucketCount = 3
+	a.low = 4
+	a.high = 5
+	a.sigfigs = 1
+	a.buckets = []*HDRHistogram{}
+	a.clock = s.tm
+
+	b := a.Export()
+	a.idx = 11
+	a.lastRoll = time.Now().Add(1 * time.Minute)
+	a.period = 12 * time.Second
+	a.bucketCount = 13
+	a.low = 14
+	a.high = 15
+	a.sigfigs = 1
+	a.buckets = nil
+	a.clock = nil
+
+	c.Assert(b.idx, Equals, 1)
+	c.Assert(b.lastRoll, Equals, origTime)
+	c.Assert(b.period, Equals, 2*time.Second)
+	c.Assert(b.bucketCount, Equals, 3)
+	c.Assert(b.low, Equals, int64(4))
+	c.Assert(b.high, Equals, int64(5))
+	c.Assert(b.buckets, NotNil)
+	c.Assert(b.clock, NotNil)
 }
