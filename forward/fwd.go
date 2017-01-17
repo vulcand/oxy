66022f22a2eea406796021c02a2a3f0ec09ceabf
Archis Gore <archis@concurix.com>
2017-01-17 15:46:03 -0800

    Change scheme for WS requests when backends might be registered as HTTP.
    Because of the way backends might be registered to Vulcan,
    and if a backend supports both http and web socket protocols,
    it makes sense for the web socket forwarder to rewrite the
    scheme correctly.
    
    It IS possible to do this with multiple routes, but that's fickle
    and difficult to maintain when not necessary.

--- forward/fwd.go
+++ forward/fwd.go
@@ -18,6 +18,7 @@ import (
 	"github.com/vulcand/oxy/utils"
 	"net/http/httputil"
 	"reflect"
+	"fmt"
 )
 
 // ReqRewriter can alter request headers and body
@@ -309,6 +310,8 @@ func (f *websocketForwarder) serveHTTP(w http.ResponseWriter, req *http.Request,
 	defer underlyingConn.Close()
 	defer targetConn.Close()
 
+	log.Infof("Writing outgoing Websocket request to target connection: %+v", outReq)
+
 	// write the modified incoming request to the dialed connection
 	if err = outReq.Write(targetConn); err != nil {
 		log.Errorf("Unable to copy request to target: %v", err)
@@ -322,7 +325,7 @@ func (f *websocketForwarder) serveHTTP(w http.ResponseWriter, req *http.Request,
 	}
 	go replicate(targetConn, underlyingConn)
 	go replicate(underlyingConn, targetConn)
-	<-errc
+	<- errc
 }
 
 // copyRequest makes a copy of the specified request.
@@ -330,8 +333,20 @@ func (f *websocketForwarder) copyRequest(req *http.Request) (outReq *http.Reques
 	outReq = new(http.Request)
 	*outReq = *req
 	outReq.URL = utils.CopyURL(req.URL)
+
+	//a good working default
 	outReq.URL.Scheme = req.URL.Scheme
+
+	//sometimes backends might be registered as HTTP/HTTPS servers so translate URLs to websocket URLs.
+	switch req.URL.Scheme {
+	case "https":
+		outReq.URL.Scheme = "wss"
+	case "http":
+		outReq.URL.Scheme = "ws"
+	}
+
 	outReq.URL.Host = req.URL.Host
+	outReq.URL.Path = req.RequestURI
 	return outReq
 }
 
