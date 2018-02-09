2e9c4dcd5864e526612960c0ac7fe6e9375ba309
Fernandez Ludovic <lfernandez.dev@gmail.com>
2018-02-09 17:01:21 +0100

    refactor: change logs level.

--- forward/fwd.go
+++ forward/fwd.go
@@ -439,14 +439,14 @@ func (f *httpForwarder) serveHTTP(w http.ResponseWriter, inReq *http.Request, ct
 	revproxy.ServeHTTP(pw, outReq)
 
 	if inReq.TLS != nil {
-		f.log.Infof("vulcand/oxy/forward/http: Round trip: %v, code: %v, Length: %v, duration: %v tls:version: %x, tls:resume:%t, tls:csuite:%x, tls:server:%v",
+		f.log.Debugf("vulcand/oxy/forward/http: Round trip: %v, code: %v, Length: %v, duration: %v tls:version: %x, tls:resume:%t, tls:csuite:%x, tls:server:%v",
 			inReq.URL, pw.Code, pw.Length, time.Now().UTC().Sub(start),
 			inReq.TLS.Version,
 			inReq.TLS.DidResume,
 			inReq.TLS.CipherSuite,
 			inReq.TLS.ServerName)
 	} else {
-		f.log.Infof("vulcand/oxy/forward/http: Round trip: %v, code: %v, Length: %v, duration: %v",
+		f.log.Debugf("vulcand/oxy/forward/http: Round trip: %v, code: %v, Length: %v, duration: %v",
 			inReq.URL, pw.Code, pw.Length, time.Now().UTC().Sub(start))
 	}
 }
