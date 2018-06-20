05e137d6e5f2750d5d9b50f3c2be6b64576dec41
herver <herver@users.noreply.github.com>
2018-06-20 09:51:21 +0200

    Send "Retry-After" to comply with RFC6585
    Also use Golang const for HTTP status

--- ratelimit/tokenlimiter.go
+++ ratelimit/tokenlimiter.go
@@ -180,8 +180,8 @@ type RateErrHandler struct {
 
 func (e *RateErrHandler) ServeHTTP(w http.ResponseWriter, req *http.Request, err error) {
 	if rerr, ok := err.(*MaxRateError); ok {
-		w.Header().Set("X-Retry-In", rerr.delay.String())
-		w.WriteHeader(429)
+		w.Header().Set("Retry-After", fmt.Sprintf("%.0f", rerr.delay.Seconds()))
+		w.WriteHeader(http.StatusTooManyRequests)
 		w.Write([]byte(err.Error()))
 		return
 	}
