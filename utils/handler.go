ad6d4af9565b939a42961fda1a0c9e337bba5603
jakub.coufal <jakub.coufal@wandera.com>
2018-08-15 10:25:04 +0200

    Report 499 for ContextCancelled added status text

--- utils/handler.go
+++ utils/handler.go
@@ -12,6 +12,9 @@ import (
 // ClientClosedRequest non-standard HTTP status code for client disconnection
 const ClientClosedRequest = 499
 
+// StatusClientClosedRequest non-standard HTTP status for client disconnection
+const StatusClientClosedRequest = "Client Closed Request"
+
 // ErrorHandler error handler
 type ErrorHandler interface {
 	ServeHTTP(w http.ResponseWriter, req *http.Request, err error)
@@ -25,20 +28,27 @@ type StdHandler struct{}
 
 func (e *StdHandler) ServeHTTP(w http.ResponseWriter, req *http.Request, err error) {
 	statusCode := http.StatusInternalServerError
+	statusText := http.StatusText(statusCode)
+
 	if e, ok := err.(net.Error); ok {
 		if e.Timeout() {
 			statusCode = http.StatusGatewayTimeout
+			statusText = http.StatusText(statusCode)
 		} else {
 			statusCode = http.StatusBadGateway
+			statusText = http.StatusText(statusCode)
 		}
-	} else if err == context.Canceled {
-		statusCode = ClientClosedRequest
 	} else if err == io.EOF {
 		statusCode = http.StatusBadGateway
+		statusText = http.StatusText(statusCode)
+	} else if err == context.Canceled {
+		statusCode = ClientClosedRequest
+		statusText = StatusClientClosedRequest
 	}
+
 	w.WriteHeader(statusCode)
-	w.Write([]byte(http.StatusText(statusCode)))
-	log.Debugf("'%d %s' caused by: %v", statusCode, http.StatusText(statusCode), err)
+	w.Write([]byte(statusText))
+	log.Debugf("'%d %s' caused by: %v", statusCode, statusText, err)
 }
 
 // ErrorHandlerFunc error handler function type
