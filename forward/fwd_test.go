6b5fc980479afc1e3c3ecf1fe4319fefc16e523f
Romain <rtribotte@users.noreply.github.com>
2021-02-26 11:10:49 +0100

    Bump to go1.16 (#214)

--- forward/fwd_test.go
+++ forward/fwd_test.go
@@ -2,7 +2,6 @@ package forward
 
 import (
 	"context"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -329,31 +328,6 @@ func TestForwardedProto(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusOK, re.StatusCode)
 	assert.Equal(t, "https", proto)
-}
-
-func TestChunkedResponseConversion(t *testing.T) {
-	srv := testutils.NewHandler(func(w http.ResponseWriter, req *http.Request) {
-		h := w.(http.Hijacker)
-		conn, _, _ := h.Hijack()
-		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n4\r\ntest\r\n5\r\ntest1\r\n5\r\ntest2\r\n0\r\n\r\n")
-		conn.Close()
-	})
-	defer srv.Close()
-
-	f, err := New()
-	require.NoError(t, err)
-
-	proxy := testutils.NewHandler(func(w http.ResponseWriter, req *http.Request) {
-		req.URL = testutils.ParseURI(srv.URL)
-		f.ServeHTTP(w, req)
-	})
-	defer proxy.Close()
-
-	re, body, err := testutils.Get(proxy.URL)
-	require.NoError(t, err)
-	assert.Equal(t, "testtest1test2", string(body))
-	assert.Equal(t, http.StatusOK, re.StatusCode)
-	assert.Equal(t, fmt.Sprintf("%d", len("testtest1test2")), re.Header.Get("Content-Length"))
 }
 
 func TestContextWithValueInErrHandler(t *testing.T) {
