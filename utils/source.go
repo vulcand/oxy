75f5879fa773aa28ce3319b3297e5cfea7049f16
gilevy <gilevy@microsoft.com>
2021-03-25 14:40:39 +0200

    add request path extractor method

--- utils/source.go
+++ utils/source.go
@@ -3,7 +3,11 @@ package utils
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // SourceExtractor extracts the source from the request, e.g. that may be client ip, or particular header that
@@ -28,6 +32,20 @@ type ExtractSource func(req *http.Request)
 func NewExtractor(variable string) (SourceExtractor, error) {
 	if variable == "client.ip" {
 		return ExtractorFunc(extractClientIP), nil
+	}
+	if strings.HasPrefix(variable, "client.ip.api.") {
+		apiRegex := strings.TrimPrefix(variable, "client.ip.api.(")
+		if len(apiRegex) == 0 {
+			return nil, fmt.Errorf("api extraction regex not provided")
+		}
+
+		reg := strings.TrimSuffix(apiRegex, ")")
+		lambda := makeClientIPWithPathExtractor(reg)
+		if lambda == nil {
+			return nil, fmt.Errorf("Failed to compile regex: %s", reg)
+		}
+
+		return lambda, nil
 	}
 	if variable == "request.host" {
 		return ExtractorFunc(extractHost), nil
@@ -57,5 +75,37 @@ func extractHost(req *http.Request) (string, int64, error) {
 func makeHeaderExtractor(header string) SourceExtractor {
 	return ExtractorFunc(func(req *http.Request) (string, int64, error) {
 		return req.Header.Get(header), 1, nil
+	})
+}
+
+//Create regex extractor function from client ip +request path
+func makeClientIPWithPathExtractor(extractRegex string) SourceExtractor {
+	compiled, err := regexp.Compile(extractRegex)
+	if err != nil {
+		return nil
+	}
+
+	return ExtractorFunc(func(req *http.Request) (string, int64, error) {
+		vals := strings.SplitN(req.RemoteAddr, ":", 2)
+		if len(vals[0]) == 0 {
+			return "", 0, fmt.Errorf("failed to parse client IP: %v", req.RemoteAddr)
+		}
+
+		client := vals[0]
+		u, err := url.Parse(req.RequestURI)
+		if err != nil {
+			return "", 0, fmt.Errorf("Failed to parse URI")
+		}
+
+		api := compiled.FindString(u.Path)
+		ret := client + api
+		if log.StandardLogger().Level >= log.DebugLevel {
+			fields := log.Fields{}
+			fields["api"] = api
+			fields["client"] = client
+			logEntry := log.StandardLogger().WithFields(fields)
+			logEntry.Debug("vulcand/oxy/SourceExtractor: extracted API")
+		}
+		return ret, 1, nil
 	})
 }
