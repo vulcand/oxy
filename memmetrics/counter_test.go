a0bcd32a4d0a929b22d9f12393fc2e3fd45798f0
Mike Kao <mkao.bao@gmail.com>
2024-11-14 21:52:20 +0800

    fix(memmetrics): prevent 'now' from accumulating during cleanup (#245)
    Co-authored-by: Fernandez Ludovic <ldez@users.noreply.github.com>

--- memmetrics/counter_test.go
+++ memmetrics/counter_test.go
@@ -1,6 +1,7 @@
 package memmetrics
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +27,26 @@ func TestCloneExpired(t *testing.T) {
 	out := cnt.Clone()
 
 	assert.EqualValues(t, 2, out.Count())
+}
+
+func Test_cleanup(t *testing.T) {
+	clock.Freeze(clock.Date(2012, 3, 4, 5, 6, 7, 0, clock.UTC))
+
+	cnt, err := NewCounter(10, clock.Second)
+	require.NoError(t, err)
+
+	cnt.Inc(1)
+
+	for i := 0; i < 9; i++ {
+		clock.Advance(clock.Second)
+		cnt.Inc(int(math.Pow10(i + 1)))
+	}
+
+	assert.EqualValues(t, 1111111111, cnt.Count())
+	assert.Equal(t, []int{1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 1, 10, 100}, cnt.values)
+
+	clock.Advance(9 * clock.Second)
+
+	assert.EqualValues(t, 1000000000, cnt.Count())
+	assert.Equal(t, []int{0, 0, 0, 0, 0, 0, 1000000000, 0, 0, 0}, cnt.values)
 }
