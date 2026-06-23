743791839522bb9d1f733708be6e5f67c64308eb
Ludovic Fernandez <ldez@users.noreply.github.com>
2026-06-23 03:36:10 +0200

    refactor(cbreaker): simplify code related to predicate by using generics (#255)

---
+++ cbreaker/predicates_functions.go
@@ -0,0 +1,31 @@
+package cbreaker
+
+import (
+	"github.com/vulcand/oxy/v2/internal/holsterv4/clock"
+)
+
+type toType[T int | float64] func(c *CircuitBreaker) T
+
+func latencyAtQuantile(quantile float64) toType[int] {
+	return func(c *CircuitBreaker) int {
+		h, err := c.metrics.LatencyHistogram()
+		if err != nil {
+			c.log.Error("Failed to get latency histogram, for %v error: %v", c, err)
+			return 0
+		}
+
+		return int(h.LatencyAtQuantile(quantile) / clock.Millisecond)
+	}
+}
+
+func networkErrorRatio() toType[float64] {
+	return func(c *CircuitBreaker) float64 {
+		return c.metrics.NetworkErrorRatio()
+	}
+}
+
+func responseCodeRatio(startA, endA, startB, endB int) toType[float64] {
+	return func(c *CircuitBreaker) float64 {
+		return c.metrics.ResponseCodeRatio(startA, endA, startB, endB)
+	}
+}
